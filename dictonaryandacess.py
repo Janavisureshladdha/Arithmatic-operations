38e38fb3d39b32dccc6d660b67d4ce91a55a8f19
Janavisureshladdha <janaviladdha@gmail.com>
2025-11-18 14:44:22 +0530

    Add files via upload

---
+++ dictonaryandacess.py
@@ -0,0 +1,6 @@
+studentinfo ={"name":"allice","age":"18","course":["maths","python","fcs"],"is_enrolled":"true"}
+print(studentinfo["name"])
+print(studentinfo["age"])
+print(studentinfo["course"])
+print(studentinfo["is_enrolled"])
+print(studentinfo["name"])
