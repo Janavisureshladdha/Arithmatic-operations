bae7fdc445543ce58406f5141938e4a8f362ef21
Janavisureshladdha <janaviladdha@gmail.com>
2025-11-21 22:09:12 +0530

    Add files via upload

---
+++ functionvowel.py
@@ -0,0 +1,8 @@
+Alphabet ="e"
+def vowel_const(Alphabet) :
+    if(Alphabet=="a"or Alphabet=="e"or Alphabet=="i"or Alphabet=="o"or Alphabet=="u") :
+        print("IS A VOWEL")
+    else :
+        print("IS A CONSONANT ")
+b =vowel_const(Alphabet)
+print(b)
