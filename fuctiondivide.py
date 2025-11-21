ea127f0a668a375147f8c055b9b7d31f69235a4a
Janavisureshladdha <janaviladdha@gmail.com>
2025-11-21 18:54:59 +0530

    Add files via upload

---
+++ fuctiondivide.py
@@ -0,0 +1,7 @@
+num1 =625
+num2 =25
+def divide_num(num1,num2) :
+    a =num1/num2
+    return a
+b=divide_num(num1,num2)
+print(b)
---
+++ functionsub.py
@@ -0,0 +1,7 @@
+num1 =5
+num2 =2
+def sub_num(num1,num2) :
+    a =num1-num2
+    return a
+b=sub_num(num1,num2)
+print(b)
