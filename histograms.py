dfb69508f620e2ae08b4f9729a62fc2e28762d6a
Janavisureshladdha <janaviladdha@gmail.com>
2025-12-16 23:10:05 +0530

    Add files via upload

---
+++ histograms.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+arr =np.array([1,3,23,5,4,6,5,64,55,43,23,2,12,67,56,15])
+plt.hist(arr,bins=[10,20,30,40,50])
+plt.xlabel
+plt.ylabel
+plt.title("histogram")
+plt.show()
